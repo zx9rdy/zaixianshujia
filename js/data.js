24befc17b2b22b8d9ee078206662e5be3af7f545
zx9rdy <129975877+zx9rdy@users.noreply.github.com>
2023-06-12 22:43:19 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var data = {
+    "count": 25,
+    "start": 1,
+    "total": 4,
+    "books": [
+    {
+        "alt": './data/行测书架/行测书架.html',
+        "author": ["ZX"],
+        "image": "./data/img/1.jpg", 
+        "publisher": "2022年9月10日",
+        "title": "行测书架",          
+    },
+    {
+        "alt": './data/申论书架/申论书架.html',
+        "author": ["ZX"],
+        "image": "./data/img/2.jpg", 
+        "publisher": "2022年9月11日",
+        "title": "申论书架",          
+    },
+    {
+        "alt": './data/教综书架/教综书架.html',
+        "author": ["ZX"],
+        "image": "./data/img/2.jpg", 
+        "publisher": "2022年9月11日",
+        "title": "教综书架",          
+    },
+    {
+        "alt": './data/信息技术书架/信息技术书架',
+        "author": ["ZX"],
+        "image": "./data/img/3.jpg", 
+        "publisher": "2022年9月12日",
+        "title": "信息技术书架",          
+    }
+]
+
+};
+
---
+++ js/jquery-1.10.2.js
@@ -0,0 +1,3 @@
+document.write("<script src='js/jquery.min.js'><\/script>");
+
+/*https://libs.baidu.com/jquery/1.10.2/jquery.min.js*/
